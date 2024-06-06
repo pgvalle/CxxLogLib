424396c360fbf92f2841fef692c70de245a0514e
pgvalle <pgvalle.nogueira@gmail.com>
2024-06-06 13:51:38 -0300

    new test program

--- test.c
+++ test.c
@@ -2,33 +2,21 @@
 #include <time.h>
 #include <stdlib.h>
 
-void delay(int number_of_seconds)
-{
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
- 
-    // Storing start time
-    clock_t start_time = clock();
- 
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds);
-}
-
 int main()
 {
   CLL_init();
   CLL_setColors(false);
-
+  
   FILE *logf = CLL_createLogFile("hellolog");
-  CLL_createLogFile("aaa");
-  delay(1);
-  CLL_createLogFile("lkajsd");
+  CLL_setLogStream(logf);
 
   CLL_info("file not found");
   CLL_debug("file not found");
-  CLL_warn("file not found");
+  CLL_warning("file not found");
   CLL_error("file not found");
   CLL_fatal("file not found");
+
+  fclose(logf);
 
   return 0;
 }
