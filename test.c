38d0a7ad99ef152c36958bdbac306193eb8faede
pgvalle <pgvalle.nogueira@gmail.com>
2024-06-06 10:10:05 -0300

    tests

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
 #include "CxxLogLib.h"
+#include <time.h>
+#include <stdlib.h>
+
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+ 
+    // Storing start time
+    clock_t start_time = clock();
+ 
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
 
 int main()
 {
   CLL_init();
+  CLL_setColors(false);
+
+  FILE *logf = CLL_createLogFile("hellolog");
+  CLL_createLogFile("aaa");
+  delay(1);
+  CLL_createLogFile("lkajsd");
 
   CLL_info("file not found");
   CLL_debug("file not found");
